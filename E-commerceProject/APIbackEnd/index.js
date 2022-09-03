baba8e569bfc11dbda18e14515e6a980d594d2c9
Mina <minaabdelmalak2016@gmail.com>
2022-09-03 15:18:16 +0200

    done

--- E-commerceProject/APIbackEnd/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const app = require("./src/app")
-
-
-const port = process.env.port || 3000
-
-
-app.listen(port , ()=> console.log(`we are on http://localhost:${port}`))
